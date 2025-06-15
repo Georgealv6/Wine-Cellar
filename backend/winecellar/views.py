b510a962a856219736a2b87fcff3e64762c6ba89
George Alvarado <georgealvarado305@gmail.com>
2025-06-14 21:41:36 -0400

    contact added to view

--- backend/winecellar/views.py
+++ backend/winecellar/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .forms import Contactform
+from django.core.mail import send_mail
 
 # welcome page
 
@@ -9,4 +11,25 @@ def homepage(request):
     return render(request, 'landing/home.html')
 
 def contactpage(request):
-    return render(request, 'landing/contact.html')
+    submitted = False
+
+    if request.method == 'POST':
+        form = Contactform(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+
+            send_mail(
+                f"New Contact Message from {name}",
+                message,
+                email,
+                ['your@email.com'],  # Replace with your email
+                fail_silently=False,
+            )
+            return render(request, 'landing/contact.html', {'form': Contactform(), 'submitted': True})
+    else:
+        form = Contactform()
+    
+
+    return render(request, 'landing/contact.html',{'form': form, 'submitted': submitted})
