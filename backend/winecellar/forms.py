06f1f6fcc190c80ac0b9c425185393da5bcf4508
George Alvarado <georgealvarado305@gmail.com>
2025-07-09 08:26:25 -0400

    conntact form updated

--- backend/winecellar/forms.py
+++ backend/winecellar/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class Contactform(forms.Form):
-    first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'placeholder':'First Name'}))
-    last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'placeholder':'Last Name'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder':'Enter Email Here'}))
-    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder':'Your Message'}))
+    first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'placeholder':'First Name','class':'wine-input'}))
+    last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'placeholder':'Last Name','class':'wine-input'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder':'Enter Email Here', 'class':'wine-input'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder':'Your Message','class':'wine-input', 'rows': 5}))
