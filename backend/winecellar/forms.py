3fc0aee5d619889ff4babdd155b3cd45db4b6042
George Alvarado <georgealvarado305@gmail.com>
2025-07-09 23:57:33 -0400

    conntact form style updated

--- backend/winecellar/forms.py
+++ backend/winecellar/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class Contactform(forms.Form):
-    first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'placeholder':'First Name','class':'wine-input'}))
-    last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'placeholder':'Last Name','class':'wine-input'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder':'Enter Email Here', 'class':'wine-input'}))
-    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder':'Your Message','class':'wine-input', 'rows': 5}))
+    first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'placeholder':'First Name','class':'wine-input','type':'text'}))
+    last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'placeholder':'Last Name','class':'wine-input', 'type':'text'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder':'Enter Email Here', 'class':'wine-input','type':'email'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder':'Your Message','class':'wine-textarea', 'rows': 5}))
