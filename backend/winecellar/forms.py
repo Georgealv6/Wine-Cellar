d3f77c4c4f22f55fe3a99cf669d151c143391452
George Alvarado <georgealvarado305@gmail.com>
2025-09-07 19:13:58 -0400

    update on html

--- backend/winecellar/forms.py
+++ backend/winecellar/forms.py
@@ -5,3 +5,7 @@ class Contactform(forms.Form):
     last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'placeholder':'Last Name','class':'wine-input', 'type':'text'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder':'Enter Email Here', 'class':'wine-input','type':'email'}))
     message = forms.CharField(widget=forms.Textarea(attrs={'placeholder':'Your Message','class':'wine-textarea', 'rows': 5}))
+
+class SignIn(forms.Form):
+
+class SignUp(forms.Form):      
